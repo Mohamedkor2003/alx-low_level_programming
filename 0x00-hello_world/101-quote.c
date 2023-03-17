91026623d109edb54e8b3a9417fd56b3ed3318a5
Mohamedkor2003 <mohameddkoraichi6635@gmail.com>
2023-03-16 18:31:58 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 1 when successful
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, strlen(msg));
 	return (1);
 }
